e7162ad5875d539032ddd3a4a1a62822f19bd1a4
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2025-04-18 21:59:33 +0100

    fix the loader

---
+++ src/components/tools/PageLoader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageLoader() {
+  return (
+    <div className="text-center h-full w-full flex justify-center items-center"><span className="mb-25 text-2xl text-inter">Loading ...</span></div>
+  )
+}
+
+export default PageLoader
