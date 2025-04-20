182659c76bb383fd5de983a0e793f19dad81b4db
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2025-04-20 15:14:11 +0100

    finish picture uploads

--- src/components/tools/PhotoDisplayer.jsx
+++ src/components/tools/PhotoDisplayer.jsx
@@ -3,6 +3,11 @@ import { Modal, Box } from '@mui/material';
 import { MdClose } from "react-icons/md";
 
 const PhotoDisplayer = ({ open, onClose, imageSrc }) => {
+  // This function will stop the click event from bubbling to the outer container
+  const stopPropagation = (e) => {
+    e.stopPropagation();
+  };
+
   return (
     <Modal
       open={open}
@@ -10,11 +15,12 @@ const PhotoDisplayer = ({ open, onClose, imageSrc }) => {
       BackdropProps={{
         sx: {
           backgroundColor: 'transparent',
-          backdropFilter: 'blur(6px)', 
+          backdropFilter: 'blur(6px)',
         },
       }}
     >
       <Box
+        onClick={onClose} // clicking on the backdrop closes
         sx={{
           position: 'absolute',
           top: '50%',
@@ -31,20 +37,23 @@ const PhotoDisplayer = ({ open, onClose, imageSrc }) => {
         }}
         className="relative w-full h-full"
       >
-  
+        {/* Close Button */}
         <span
-          className="absolute top-5  right-10 cursor-pointer p-2 aspect-square rounded-full text-3xl text-gray-700 bg-gray-200 "
-          onClick={onClose}
+          className="absolute top-5 right-10 cursor-pointer p-2 aspect-square rounded-full text-3xl text-gray-700 bg-gray-200"
+          onClick={(e) => {
+            e.stopPropagation();
+            onClose();
+          }}
         >
           <MdClose />
         </span>
 
-        {/* Image */}
-        <div className="relative p-10">
+        {/* Image (clicking here won’t close modal) */}
+        <div className="relative p-10" onClick={stopPropagation}>
           <img
             src={imageSrc}
             alt="Displayed"
-            className="object-contain max-w-full max-h-full"
+            className="object-contain max-w-full max-h-full rounded-3xl"
           />
         </div>
       </Box>
