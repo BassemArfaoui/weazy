7f12df8064ec3c29be634372fa2ab9f54c1879e9
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2025-04-26 20:17:26 +0100

    fix a small ui problem

--- src/components/main/chat/Welcome.jsx
+++ src/components/main/chat/Welcome.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Welcome() {
   return (
-    <div className='flex flex-col items-center justify-center gap-2  mb-9'>
-      <div className="text-gray-300 text-2xl text-center">
-        Hello <span className="text-white">Bassem</span>,
+    <div className='flex flex-col items-center justify-center gap-1  mb-7'>
+      <div className="text-gray-300 text-xl text-center">
+        Hello <span className="text-white">Bassem </span>,
       </div>
-      <div className="text-gray-300 text-center">
+      <div className="text-gray-300 text-center text-2xl">
         What do you want to find today?
       </div>
     </div>
