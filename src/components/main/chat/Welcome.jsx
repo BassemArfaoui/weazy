5f3272dc035cdcfcede0ab5702908620efbc3cba
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2025-09-24 16:07:29 +0100

    small change for phones

--- src/components/main/chat/Welcome.jsx
+++ src/components/main/chat/Welcome.jsx
@@ -6,7 +6,7 @@ function Welcome() {
       <div className="text-gray-300 text-xl text-center">
         Hello <span className="text-title">Bassem </span>,
       </div>
-      <div className="text-border-100 text-center text-2xl px-3">
+      <div className="text-border-100 text-center text-xl md:text-2xl px-3">
         What do you want to find today?
       </div>
     </div>
